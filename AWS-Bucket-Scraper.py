c2e22a61f8e11a95ea6be6e4db03aae5be828232
JLovikSecurity <110497415+JLovikSecurity@users.noreply.github.com>
2023-11-03 13:52:57 -0600

    Update AWS-Bucket-Scraper.py
    Updated with extension statistics output

--- AWS-Bucket-Scraper.py
+++ AWS-Bucket-Scraper.py
@@ -1,30 +1,61 @@
 import boto3
 import botocore
+import os
 
-# List of public S3 buckets
-public_buckets = ["MyBucket"]  # Replace with your bucket names
+# List of public S3 buckets along with their regions
+public_buckets = [("myBucket1", "us-east-1"), ("myBucket2", "us-east-1")]
 
-def list_files_in_bucket(bucket_name):
+def list_files_in_bucket(bucket_name, region, output_file, extension_statistics):
     try:
         # Create a session without providing credentials
         session = boto3.Session()
         config = botocore.config.Config(signature_version=botocore.UNSIGNED)
-        s3 = session.client('s3', region_name='us-east-1', config=config)  # Specify your region
+        s3 = session.client('s3', region_name=region, config=config)
 
         # List objects in the bucket
         objects = s3.list_objects_v2(Bucket=bucket_name)
 
-        for obj in objects.get('Contents', []):
-            file_key = obj['Key']
-            file_size_mb = obj['Size'] / (1024 * 1024)
-            print(f"Bucket: {bucket_name}, File: {file_key}, Size: {file_size_mb:.2f} MB")
+        extension_count = {}  # Dictionary to count file extensions
+
+        with open(output_file, 'a') as file:  # Open the file in append mode
+            for obj in objects.get('Contents', []):
+                file_key = obj['Key']
+                file_size_mb = obj['Size'] / (1024 * 1024)
+                print(f"Bucket: {bucket_name}, Region: {region}, File: {file_key}, Size: {file_size_mb:.2f} MB\n")
+                file.write(f"Bucket: {bucket_name}, Region: {region}, File: {file_key}, Size: {file_size_mb:.2f} MB\n")
+                # Count file extensions
+                _, file_extension = os.path.splitext(file_key)
+                if file_extension:
+                    extension_count[file_extension] = extension_count.get(file_extension, 0) + 1
+
+        # Close the 'with' block for objects here
+
+        # Update the extension statistics dictionary
+        for extension, count in extension_count.items():
+            extension_statistics[extension] = extension_statistics.get(extension, 0) + count
 
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'NoSuchBucket':
-            print(f"Bucket: {bucket_name} not found.")
+            with open(output_file, 'a') as file:
+                print(f"Bucket: {bucket_name} not found in region {region}.\n")
+                file.write(f"Bucket: {bucket_name} not found in region {region}.\n")
         else:
-            print(f"Bucket: {bucket_name}, An error occurred: {e}")
+            with open(output_file, 'a') as file:
+                print(f"Bucket: {bucket_name}, An error occurred in region {region}: {e}\n")
+                file.write(f"Bucket: {bucket_name}, An error occurred in region {region}: {e}\n")
 
-if __name__ == '__main__':
-    for bucket in public_buckets:
-        list_files_in_bucket(bucket)
+output_file = 'AWS-Bucket-Scraper.txt'  # Specify the name of the output file
+with open(output_file, 'w') as file:  # Create or clear the output file
+    file.write('')  # Clear the file
+
+extension_statistics = {}  # Dictionary to collect extension statistics
+
+for bucket, region in public_buckets:
+    list_files_in_bucket(bucket, region, output_file, extension_statistics)
+
+# Print file extension statistics across all buckets
+with open(output_file, 'a') as file:  # Open the file in append mode
+    file.write("\nFile Extension Statistics Across All Buckets:\n")
+    for extension, count in extension_statistics.items():
+        print(f"Extension: {extension}, Count: {count}\n")
+        file
